cecd91e7a455a74ba1b17807c1c8522ec4380cea
HajarAlnasser <86411115+HajarAlnasser@users.noreply.github.com>
2022-08-30 22:18:16 +0300

    fix 2

--- Assets/testConflict.cs
+++ Assets/testConflict.cs
@@ -13,7 +13,9 @@ public class testConflict : MonoBehaviour
         print("gintama is less fun than one piece ");
 //=======
         print("one piece is best anime");
-//>>>>>>> d1e3cbca277369a44988941edb48308c36c15c15
+        print("gintama better than one piece ");
+        print("gintama is the only ONE ");
+        //>>>>>>> d1e3cbca277369a44988941edb48308c36c15c15
     }
 
     // Update is called once per frame
