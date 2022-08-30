53b9608ef4e065e1422bd1b12db05797ba5c7898
Hanan-cloud <79918951+Hanan-cloud@users.noreply.github.com>
2022-08-30 22:07:42 +0300

    fix conflict

--- Assets/testConflict.cs
+++ Assets/testConflict.cs
@@ -9,11 +9,11 @@ public class testConflict : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
-        print("gintama better than one peice ");
-=======
-        print("one pice best anime");
->>>>>>> d1e3cbca277369a44988941edb48308c36c15c15
+//<<<<<<< HEAD
+        print("gintama better than one piece ");
+//=======
+        print("one piece not best anime");
+//>>>>>>> d1e3cbca277369a44988941edb48308c36c15c15
     }
 
     // Update is called once per frame
