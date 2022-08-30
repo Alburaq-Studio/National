d1e3cbca277369a44988941edb48308c36c15c15
HajarAlnasser <86411115+HajarAlnasser@users.noreply.github.com>
2022-08-30 22:03:00 +0300

    conflict

--- Assets/testConflict.cs
+++ Assets/testConflict.cs
@@ -9,7 +9,7 @@ public class testConflict : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("gintama best anime");
+        print("one pice best anime");
     }
 
     // Update is called once per frame
